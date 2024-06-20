d522876ccb185990da268d4413707a3e2498f78a
Sajid al-Islam <saajiidi@gmail.com>
2024-06-20 18:57:39 +0600

    Update axios.ts

--- src/lib/util/axios.ts
+++ src/lib/util/axios.ts
@@ -2,8 +2,21 @@
 
 import axios from "axios"
 
+
+// Axios instance with base URL from environment variable or default
+const baseURL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://medusa-backend-black.vercel.app"
+
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL,
+  // Other Axios configurations if needed
 })
 
-export default axiosInstance
+module.exports = {
+  // Your Next.js configuration
+  env: {
+    API_BASE_URL: baseURL,
+    // Other environment variables
+  },
+}
+
